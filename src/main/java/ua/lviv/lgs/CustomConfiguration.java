7b382751959a73a44b61b155f62cf07f7ca773c2
Андрій <@DESKTOP-M7IR61T>
2019-05-17 15:37:45 +0300

    init

--- src/main/java/ua/lviv/lgs/CustomConfiguration.java
+++ src/main/java/ua/lviv/lgs/CustomConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import ua.lviv.lgs.model.Student;
-import ua.lviv.lgs.repository.impl.StudentRepository;
 
 @Configuration
 public class CustomConfiguration {
@@ -12,11 +11,6 @@ public class CustomConfiguration {
 	@Bean(name = "student")
 	public Student getStudent() {
 		return new Student(0, "Student", 20);
-	}
-
-	@Bean(name = "studentRepository")
-	public StudentRepository<Student> getStudentRepository() {
-		return new StudentRepository<Student>();
 	}
 
 }
