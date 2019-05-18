62da44a17474069c6143cfde3a2b5fdf26ce4698
Андрій <@DESKTOP-M7IR61T>
2019-05-19 00:41:02 +0300

    init

--- src/main/java/ua/lviv/lgs/SpringBootDemoApplication.java
+++ src/main/java/ua/lviv/lgs/SpringBootDemoApplication.java
@@ -7,10 +7,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 import ua.lviv.lgs.model.Student;
 
 @SpringBootApplication
-public class SpringDemoApplication {
+public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext appContext = SpringApplication.run(SpringDemoApplication.class, args);
+		ConfigurableApplicationContext appContext = SpringApplication.run(SpringBootDemoApplication.class, args);
 
 		Student student = (Student) appContext.getBean("student");
 		System.out.println(student);
